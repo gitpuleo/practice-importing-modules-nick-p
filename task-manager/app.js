8fa6b4f37d1d81c4c9ea65d74b82d6c7732ecbcd
nfp <nfpuleo@gmail.com>
2025-06-01 17:41:23 -0500

    further work in defining functions, planning out functionality implementation

--- task-manager/app.js
+++ task-manager/app.js
@@ -13,3 +13,22 @@ const path = require('path');
 
 
 */
+
+//Carrie's solution vvv
+
+const tm = require('./taskManager');
+const { saveTasks, loadTasks } = require('./fileHandler');
+const path = require('path');
+
+// Build file path
+const filepath = path.join(__dirname, 'tasks.json');
+// const filepath = __dirname + "/" + "tasks.json";
+console.log(filepath);
+
+// Load tasks from tasks.json
+let allTasksArray = loadTasks(filepath);
+console.log(allTasksArray);
+
+
+
+//v Add tasks to list to check functionality
