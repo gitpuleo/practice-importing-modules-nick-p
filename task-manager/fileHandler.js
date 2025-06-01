e0c5dda3ffe80ec91f42ad036b44194823ba71a9
nfp <nfpuleo@gmail.com>
2025-06-01 17:39:20 -0500

    Some functions defined, further functionality still needs implementing

--- task-manager/fileHandler.js
+++ task-manager/fileHandler.js
@@ -2,12 +2,24 @@ const fs = require('fs');
 
 
 function saveTasks(filepath, tasks){
-
+    let jsonTasks = JSON.stringify(tasksArray);
+	fs.writeFileSync(filePath, jsonTasks);
+	console.log('\nTasks saved to file.');
 }
+
+//^How to use the JSON object?
 
 function loadTasks(filepath){
-
+    if (fs.existsSync(filePath)) {
+const jsonData = fs.readFileSync(filePath, 'utf-8');
+		let jsData = JSON.parse(jsonData);
+		return jsData;
+	} else {
+		return [];
+	}
 }
+
+//read on JSON object methods like .parse() ^
 
 module.exports = {
     saveTasks: saveTasks,
