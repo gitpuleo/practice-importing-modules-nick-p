bd1ce88d0d3ff1b7874501dcaaefcc41d24feff9
nfp <nfpuleo@gmail.com>
2025-07-06 17:12:41 -0500

    small tweaks

--- task-manager/app.js
+++ task-manager/app.js
@@ -5,14 +5,6 @@ const path = require('path');
 
 
 
-/*
-1) load existing tasks from a file named tasks.json
-2) Add new tasks to the list
-3) List all tasks
-4) Save the updated task list back to the file
-
-
-*/
 
 //Carrie's solution vvv
 
